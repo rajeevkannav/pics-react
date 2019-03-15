3cede5330e8eec141383a3516e6391e8e5aa6d14
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-15 16:14:09 +0530

    implemented lists with the results unsplashed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
+import ImageList from './ImageList';
 
 class App extends React.Component {
 
@@ -8,9 +10,16 @@ class App extends React.Component {
 
     onSearchSubmit = async (term) => {
 
-        const response = await unsplash.get('/search/photos', {
-            params:{query: term}
-        });
+        // const response = await unsplash.get('/search/photos', {
+        //     params:{query: term}
+        // });
+
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params:{query: term},
+            headers:{
+                Authorization: "Client-ID ab90ecda4b4d1505343cf7d4a4c1c2e0170113165897d463d2dca45ca1a1f062"
+            }
+        })
 
         this.setState({images: response.data.results})
     }
@@ -19,7 +28,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images}/>
             </div>
         )
     }
